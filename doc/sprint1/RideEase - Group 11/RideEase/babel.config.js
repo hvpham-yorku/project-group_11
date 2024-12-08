85580e23daad7634d3485a96be0004a99648b7fc
Ahmad Kanoun <ahmadkanoun123@gmail.com>
2024-12-07 23:41:02 -0500

    Remove incorrect sprint1 folder

--- doc/sprint1/RideEase - Group 11/RideEase/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"], // Correctly defined presets
-    plugins: ["nativewind/babel"],  // Plugins placed in the correct property
-  };
-};
